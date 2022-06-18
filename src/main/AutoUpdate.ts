eeeb37b5966c0fd724fcb4c9dbecd3c083b2eaa5
kk <kk>
2022-06-18 17:29:33 +0100

    Fixed app not building Added Autoupdater (basic)

---
+++ src/main/AutoUpdate.ts
@@ -0,0 +1,21 @@
+import {autoUpdater} from "electron";
+import AutoUpdater = Electron.AutoUpdater;
+
+export default class AppUpdater {
+    private readonly appUpdater;
+    private readonly constants = {
+        url: 'https://antisniper.seraph.si/overlay/latest'
+    }
+
+    constructor() {
+        this.appUpdater = autoUpdater;
+        this.appUpdater.setFeedURL({
+            url: this.constants.url
+        });
+    }
+
+    getAutoUpdater(): AutoUpdater {
+        return this.appUpdater;
+    }
+
+}
