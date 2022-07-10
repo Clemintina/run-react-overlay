7cfdb2d1c29dedfdc1e1cefd7564d01ffbac3382
kk <kk>
2022-07-10 03:02:25 +0100

    Made tooltips more minecrafty

--- src/renderer/components/StatisticsTooltip.tsx
+++ src/renderer/components/StatisticsTooltip.tsx
@@ -1,4 +1,4 @@
-import React, {PropsWithChildren, useState} from "react";
+import React, {PropsWithChildren} from "react";
 import {Player} from "@common/utils/PlayerUtils";
 import Tooltip, {tooltipClasses, TooltipProps} from "@mui/material/Tooltip";
 import {styled} from "@mui/material";
@@ -15,10 +15,18 @@ export const StatsisticsTooltip: React.FC<PropsWithChildren<StatisticsTooltip>> 
 
     const CustomToolTip = styled(({className, ...props}: TooltipProps) => <Tooltip {...props} classes={{popper: className}} />)(({theme}) => ({
         [`& .${tooltipClasses.tooltip}`]: {
-            backgroundColor: colours.backgroundColour,
+            // backgroundColor: colours.backgroundColour,
+            backgroundColor: "#120211",
             color: colours.primaryColour,
+            padding: "5px",
+            paddingBottom: "3px",
+            borderRadius: "5px",
+            border: "1px solid #25015b",
+            lineHeight: "17px",
+            left: "350px",
+            paddingRight: "12px",
             maxWidth: 220,
-            fontSize: theme.typography.pxToRem(12),
+            fontSize: theme.typography.pxToRem(16),
         },
     }));
 
