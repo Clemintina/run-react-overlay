73841c961271b818124d9f100e7339626d308a19
kk <kk>
2022-07-11 09:03:01 +0100

    Added ability to toggle APIs

--- src/renderer/views/MainSettings.tsx
+++ src/renderer/views/MainSettings.tsx
@@ -1,6 +1,6 @@
 import "@assets/scss/titlebar.scss";
 import "@assets/scss/settings.scss";
-import React, {MouseEventHandler} from "react";
+import React from "react";
 import store from "@renderer/store";
 import {apiKeyValidator, keathizApiKeyValidator, setSettingsValue, SettingsConfig} from "@renderer/store/ConfigStore";
 import {useSelector} from "react-redux";
@@ -9,7 +9,6 @@ import {faExclamationCircle} from "@fortawesome/free-solid-svg-icons";
 import {UnderlinedTitle} from "@components/user/UnderlinedTitle";
 import {InputTextBox} from "@components/user/InputTextBox";
 import {InputBoxButton} from "@components/user/InputBoxButton";
-import {IPCResponse} from "@common/utils/externalapis/RunApi";
 import {ToggleButton} from "@components/user/ToggleButton";
 
 const MainSettings = () => {
@@ -18,9 +17,9 @@ const MainSettings = () => {
 
     // TODO make it look nicer and cleaner
     return (
-        <div className='mainSettingsPanel'>
+        <div className="mainSettingsPanel">
             <UnderlinedTitle text={"Overlay Settings"} options={{text: {size: 30}}} />
-            <div className='mainSettingsOption'>
+            <div className="mainSettingsOption">
                 <UnderlinedTitle text={"Hypixel API Key: "}></UnderlinedTitle>
                 <InputTextBox
                     onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -31,7 +30,7 @@ const MainSettings = () => {
                 />
                 {<FontAwesomeIcon style={{color: isHypixelKeySet ? "green" : "red"}} icon={faExclamationCircle} />}
             </div>
-            <div className='mainSettingsOption'>
+            <div className="mainSettingsOption">
                 <UnderlinedTitle text={"Keathiz Key: "}> </UnderlinedTitle>
                 <InputTextBox
                     onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -42,11 +41,11 @@ const MainSettings = () => {
                 />
                 {<FontAwesomeIcon style={{color: isHypixelKeySet ? "green" : "red"}} icon={faExclamationCircle} />}
             </div>
-            <div className='mainSettingsOption'>
+            <div className="mainSettingsOption">
                 <UnderlinedTitle text={"Overlay Logs: "}></UnderlinedTitle>
                 <InputBoxButton
                     onClick={async () => {
-                        const path:Electron.OpenDialogReturnValue = await window.ipcRenderer.invoke("selectLogFile");
+                        const path: Electron.OpenDialogReturnValue = await window.ipcRenderer.invoke("selectLogFile");
                         if (path.filePaths[0] !== undefined) {
                             const logPath = path.filePaths[0];
                             const readable = await window.ipcRenderer.invoke<boolean>("isFileReadable", logPath);
@@ -60,11 +59,25 @@ const MainSettings = () => {
                 {<FontAwesomeIcon style={{color: isLogsSet ? "green" : "red"}} icon={faExclamationCircle} />}
             </div>
             <div>
-                <ToggleButton text={'Keathiz'} onChange={ async (event)=>{
-                    const payload: SettingsConfig = {boomza: store.getState().configStore.settings.boomza, keathiz: store.getState().configStore.settings.keathiz, lunar: store.getState().configStore.settings.lunar}
-                    payload.keathiz = event.currentTarget.checked
+                <ToggleButton text={"Keathiz"} onChange={async (event) => {
+                    const payload: SettingsConfig = {boomza: store.getState().configStore.settings.boomza, keathiz: store.getState().configStore.settings.keathiz, lunar: store.getState().configStore.settings.lunar};
+                    payload.keathiz = event.currentTarget.checked;
                     store.dispatch(setSettingsValue(payload));
-                }}></ToggleButton>
+                }} />
+            </div>
+            <div>
+                <ToggleButton text={"Lunar"} onChange={async (event) => {
+                    const payload: SettingsConfig = {boomza: store.getState().configStore.settings.boomza, keathiz: store.getState().configStore.settings.keathiz, lunar: store.getState().configStore.settings.lunar};
+                    payload.lunar = event.currentTarget.checked;
+                    store.dispatch(setSettingsValue(payload));
+                }} />
+            </div>
+            <div>
+                <ToggleButton text={"Boomza"} onChange={async (event) => {
+                    const payload: SettingsConfig = {boomza: store.getState().configStore.settings.boomza, keathiz: store.getState().configStore.settings.keathiz, lunar: store.getState().configStore.settings.lunar};
+                    payload.boomza = event.currentTarget.checked;
+                    store.dispatch(setSettingsValue(payload));
+                }} />
             </div>
         </div>
     );
