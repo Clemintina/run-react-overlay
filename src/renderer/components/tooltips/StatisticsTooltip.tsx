71153eb44122c383e642d8710b4edc2216c3f1ba
kk <kk>
2022-07-11 22:38:21 +0100

    Added colours to tooltip

--- src/renderer/components/tooltips/StatisticsTooltip.tsx
+++ src/renderer/components/tooltips/StatisticsTooltip.tsx
@@ -4,6 +4,7 @@ import {getPlayerRank} from "@common/zikeji";
 import {OverlayTooltip} from "@components/tooltips/OverlayTooltip";
 import {useSelector} from "react-redux";
 import store from "@renderer/store";
+import {Interweave} from "interweave";
 
 export interface StatisticsTooltip {
     player: Player;
@@ -23,55 +24,55 @@ export const StatsisticsTooltip: React.FC<PropsWithChildren<StatisticsTooltip>> 
                 tooltip={
                     <div>
                         {player.hypixelPlayer !== null && (
-                            <span>
-                                <span style={{color: `#${getPlayerRank(player.hypixelPlayer).colourHex}`}}>{player.hypixelPlayer.displayname}</span>
+                            <div className={"statistics-tooltip"}>
+                                <span className={"statistics-tooltip-inline"} style={{color: `#${getPlayerRank(player.hypixelPlayer).colourHex}`}}>{player.hypixelPlayer.displayname}</span>
                                 <div>
-                                    <span style={{color: colours.primaryColour}}>Games Played: {playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "gamesPlayed")}</span>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>Games Played: <Interweave content={playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "gamesPlayed")} /></span>
                                 </div>
                                 <div>
-                                    <span style={{color: colours.primaryColour}}>Wins: {playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "wins")}</span>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>Wins: <Interweave content={playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "wins")} /> </span>
                                 </div>
                                 <div>
-                                    <span style={{color: colours.primaryColour}}>Losses: {playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "losses")}</span>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>Losses: <Interweave content={playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "losses")} /> </span>
                                 </div>
                                 <div>
-                                    <span style={{color: colours.primaryColour}}>WLR: {playerFormatter.getFormatPlayerInstance().renderRatioColour(player, "wlr")}</span>
-                                </div>
-                                <br />
-                                <div>
-                                    <span style={{color: colours.primaryColour}}>Final Kills: {playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "finalKills")}</span>
-                                </div>
-                                <div>
-                                    <span style={{color: colours.primaryColour}}>Final Deaths: {playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "finalDeaths")}</span>
-                                </div>
-                                <div>
-                                    <span style={{color: colours.primaryColour}}>FKDR: {playerFormatter.getFormatPlayerInstance().renderRatioColour(player, "fkdr")}</span>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>WLR: <Interweave content={playerFormatter.getFormatPlayerInstance().renderRatioColour(player, "wlr")} /> </span>
                                 </div>
                                 <br />
                                 <div>
-                                    <span style={{color: colours.primaryColour}}>Beds Broken: {playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "bedsBroken")}</span>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>Final Kills: <Interweave content={playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "finalKills")} /> </span>
                                 </div>
                                 <div>
-                                    <span style={{color: colours.primaryColour}}>Beds Lost: {playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "bedsLost")}</span>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>Final Deaths: <Interweave content={playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "finalDeaths")} /> </span>
                                 </div>
                                 <div>
-                                    <span style={{color: colours.primaryColour}}>BBLR: {playerFormatter.getFormatPlayerInstance().renderRatioColour(player, "bblr")}</span>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>FKDR: <Interweave content={playerFormatter.getFormatPlayerInstance().renderRatioColour(player, "fkdr")} /> </span>
                                 </div>
                                 <br />
                                 <div>
-                                    <span style={{color: colours.primaryColour}}>Kills: {playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "kills")}</span>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>Beds Broken: <Interweave content={playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "bedsBroken")} /> </span>
                                 </div>
                                 <div>
-                                    <span style={{color: colours.primaryColour}}>Deaths: {playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "deaths")}</span>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>Beds Lost: <Interweave content={playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "bedsLost")} /> </span>
                                 </div>
                                 <div>
-                                    <span style={{color: colours.primaryColour}}>KDR: {playerFormatter.getFormatPlayerInstance().renderRatioColour(player, "kdr")}</span>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>BBLR: <Interweave content={playerFormatter.getFormatPlayerInstance().renderRatioColour(player, "bblr")} /> </span>
                                 </div>
                                 <br />
                                 <div>
-                                    <span style={{color: colours.primaryColour}}>First Login: {playerFormatter.getPlayerHypixelUtils().getDateFormatted(player.hypixelPlayer?.lastLogin) ?? 0}</span>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>Kills: <Interweave content={playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "kills")}></Interweave></span>
                                 </div>
-                            </span>
+                                <div>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>Deaths: <Interweave content={playerFormatter.getFormatPlayerInstance().renderCoreStatsColour(player, "deaths")} /></span>
+                                </div>
+                                <div>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>KDR: <Interweave content={playerFormatter.getFormatPlayerInstance().renderRatioColour(player, "kdr")} /> </span>
+                                </div>
+                                <br />
+                                <div>
+                                    <span className={"statistics-tooltip-inline"} style={{color: colours.primaryColour}}>First Login: <Interweave content={playerFormatter.getPlayerHypixelUtils().getDateFormatted(player.hypixelPlayer?.lastLogin) ?? 0} /> </span>
+                                </div>
+                            </div>
                         )}
                     </div>
                 }
