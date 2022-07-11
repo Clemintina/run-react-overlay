07843b1a1eb84a294d1a45846371b151c3854bdf
Arman <idocorrupt@gmail.com>
2022-07-10 22:13:23 -0600

    Added player stats to tooltips. First login date has not yet been formatted.

--- src/renderer/components/tooltips/StatisticsTooltip.tsx
+++ src/renderer/components/tooltips/StatisticsTooltip.tsx
@@ -1,7 +1,7 @@
-import React, {PropsWithChildren} from "react";
-import {Player} from "@common/utils/PlayerUtils";
-import {getPlayerRank} from "@common/zikeji";
-import {OverlayTooltip} from "@components/tooltips/OverlayTooltip";
+import React, { PropsWithChildren } from "react";
+import { Player } from "@common/utils/PlayerUtils";
+import { getPlayerRank, getBedwarsLevelInfo } from "@common/zikeji";
+import { OverlayTooltip } from "@components/tooltips/OverlayTooltip";
 
 export interface StatisticsTooltip {
     player: Player;
@@ -19,9 +19,52 @@ export const StatsisticsTooltip: React.FC<PropsWithChildren<StatisticsTooltip>> 
                     <div>
                         {player.hypixelPlayer !== null && (
                             <span>
-                                <span style={{color: `#${getPlayerRank(player.hypixelPlayer).colourHex}`}}>{player.hypixelPlayer?.displayname}</span>
+                                <span style={{ color: `#${getPlayerRank(player.hypixelPlayer).colourHex}` }}>{player.hypixelPlayer?.displayname}</span>
                                 <div>
-                                    <span>Data: ....</span>
+                                    <span style={{ color: "white" }}>Games Played: {player.hypixelPlayer?.stats.Bedwars?.games_played_bedwars || 0}</span>
+                                </div>
+                                <div>
+                                    <span style={{ color: "white" }}>Wins: {player.hypixelPlayer?.stats.Bedwars?.wins_bedwars || 0}</span>
+                                </div>
+                                <div>
+                                    <span style={{ color: "white" }}>Losses: {player.hypixelPlayer?.stats.Bedwars?.losses_bedwars || 0}</span>
+                                </div>
+                                <div>
+                                    <span style={{ color: "white" }}>WLR: {((player.hypixelPlayer?.stats.Bedwars?.wins_bedwars || 0) / (player.hypixelPlayer?.stats.Bedwars?.losses_bedwars || 0)).toFixed(2)}</span>
+                                </div>
+                                <br/>
+                                <div>
+                                    <span style={{ color: "white" }}>Final Kills: {player.hypixelPlayer?.stats.Bedwars?.final_kills_bedwars || 0}</span>
+                                </div>
+                                <div>
+                                    <span style={{ color: "white" }}>Final Deaths: {player.hypixelPlayer?.stats.Bedwars?.final_deaths_bedwars || 0}</span>
+                                </div>
+                                <div>
+                                    <span style={{ color: "white" }}>FKDR: {((player.hypixelPlayer?.stats.Bedwars?.final_kills_bedwars || 0) / (player.hypixelPlayer?.stats.Bedwars?.final_deaths_bedwars || 0)).toFixed(2)}</span>
+                                </div>
+                                <br/>
+                                <div>
+                                    <span style={{ color: "white" }}>Beds Broken: {player.hypixelPlayer?.stats.Bedwars?.beds_broken_bedwars || 0}</span>
+                                </div>
+                                <div>
+                                    <span style={{ color: "white" }}>Beds Lost: {player.hypixelPlayer?.stats.Bedwars?.beds_lost_bedwars || 0}</span>
+                                </div>
+                                <div>
+                                    <span style={{ color: "white" }}>BBLR: {((player.hypixelPlayer?.stats.Bedwars?.beds_broken_bedwars || 0) / (player.hypixelPlayer?.stats.Bedwars?.beds_lost_bedwars || 0)).toFixed(2)}</span>
+                                </div>
+                                <br/>
+                                <div>
+                                    <span style={{ color: "white" }}>Kills: {player.hypixelPlayer?.stats.Bedwars?.kills_bedwars || 0}</span>
+                                </div>
+                                <div>
+                                    <span style={{ color: "white" }}>Deaths: {player.hypixelPlayer?.stats.Bedwars?.deaths_bedwars || 0}</span>
+                                </div>
+                                <div>
+                                    <span style={{ color: "white" }}>KDR: {((player.hypixelPlayer?.stats.Bedwars?.kills_bedwars || 0) / (player.hypixelPlayer?.stats.Bedwars?.deaths_bedwars || 0)).toFixed(2)}</span>
+                                </div>
+                                <br/>
+                                <div>
+                                    <span style={{ color: "white" }}>First Login: {player.hypixelPlayer?.firstLogin || 0}</span>
                                 </div>
                             </span>
                         )}
