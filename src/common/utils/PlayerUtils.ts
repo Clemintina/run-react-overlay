d8fa0568829b5e5064f6ad0c314ce82fb1300c89
kk <kk>
2022-08-28 23:14:45 +0100

    Cleaned up PlayerUtils

--- src/common/utils/PlayerUtils.ts
+++ src/common/utils/PlayerUtils.ts
@@ -2,11 +2,6 @@ import {Blacklist, IPCResponse, LunarAPIResponse} from "./externalapis/RunApi";
 import {Components} from "@common/zikeji";
 import {BoomzaAntisniper, KeathizOverlayRun} from "@common/utils/externalapis/BoomzaApi";
 import {PlayerDB} from "@common/utils/externalapis/PlayerDB";
-import {MetricsObject, TagArray, TagObject} from "@common/utils/Schemas";
-import {RUNElectronStoreTagsTyped} from "@main/appWindow";
-import jsonLogic from "json-logic-js";
-import useConfigStore from "@renderer/store/zustand/ConfigStore";
-import useTagStore from "@renderer/store/zustand/TagStore";
 
 export type Player = {
     name: string;
@@ -30,110 +25,15 @@ export type Player = {
 };
 
 export class PlayerUtils {
-    private readonly formatPlayerInstance: FormatPlayer;
     private readonly playerHypixelUtils: PlayerHypixelUtils;
 
-    constructor(stores?: {config: object; tags: object}) {
-        this.formatPlayerInstance = new FormatPlayer();
+    constructor() {
         this.playerHypixelUtils = new PlayerHypixelUtils();
-        if (stores != undefined) {
-            this.formatPlayerInstance.setConfig(stores);
-        } else {
-            this.formatPlayerInstance.setConfig({config: useConfigStore.getState(), tags: useTagStore.getState()});
-        }
-    }
-
-    public getFormatPlayerInstance() {
-        return this.formatPlayerInstance;
     }
 
     public getPlayerHypixelUtils() {
         return this.playerHypixelUtils;
     }
-}
-
-export class FormatPlayer {
-    private isOverlayStats = false;
-    private starterDivider = `<span class="${this.isOverlayStats ? "flex" : ""} text-left">`;
-    private tagStore;
-    private configStore;
-
-    public setConfig = (stores: {config: object; tags: object}, options?: {subMenu?: boolean}) => {
-        this.tagStore = stores.tags;
-        this.configStore = stores.config;
-        this.isOverlayStats = options?.subMenu ?? false;
-    };
-
-    public getTagsFromConfig = (tagDisplayPath: RUNElectronStoreTagsTyped | string, value?: number) => {
-        const tag: TagObject | undefined = tagDisplayPath.split(".").reduce((o, i) => o[i], this.tagStore);
-        const tagDisplayIcon: string | undefined = tag?.display ?? "?";
-        const tagArray: string | Array<TagArray> | undefined = tag?.colour ?? "FF5555";
-        if (tagDisplayPath == "run.blacklist") {
-            return this.getPlayerTagDivider(value, `#${tagArray}`);
-        } else if (Array.isArray(tagArray) && value != undefined) {
-            const tempArray = [...tagArray];
-            const arr = tempArray.sort((a, b) => b.requirement - a.requirement);
-            for (const {colour, requirement} of arr) {
-                if (value >= requirement) {
-                    return this.getPlayerTagDivider(tagDisplayIcon, `#${colour}`);
-                }
-            }
-        } else {
-            return this.getPlayerTagDivider(tagDisplayIcon, `#${tagArray?.toString()}`);
-        }
-        return this.getPlayerTagDivider(tagDisplayIcon, "amber");
-    };
-
-    public getCoreFromConfig = (tagDisplayPath: RUNElectronStoreTagsTyped | string, value: number) => {
-        try {
-            const coreMetric: MetricsObject | undefined = tagDisplayPath.split(".").reduce((o, i) => o[i], this.tagStore) ?? 0;
-            const coreArray = coreMetric?.colours ?? "FF5555";
-
-            if (!isFinite(value)) value = ~~Number((((0 - 18) / 0) * 100).toFixed(2));
-
-            if (Array.isArray(coreArray) && value != undefined) {
-                const tempArray = [...coreArray];
-                const arr = tempArray.sort((a, b) => a.requirement - b.requirement);
-                for (const {colour, requirement, operator} of arr) {
-                    if (jsonLogic.apply({[operator]: [value, requirement]})) {
-                        return this.getPlayerTagDivider(value, `#${colour}`);
-                    }
-                }
-                return this.getPlayerTagDivider(value, `#555555`);
-            } else {
-                return this.getPlayerTagDivider(value, `#${coreArray}`);
-            }
-        } catch (e) {
-            return this.getPlayerTagDivider(tagDisplayPath, "FF5555");
-        }
-    };
-
-    private getPlayerTagDivider = (tag: string | number | unknown, colour: string, player?: Player) => {
-        let htmlResponse = ``,
-            styleString = `color: ${colour};`,
-            classNameData = ``;
-        if (typeof tag === "number" && !Number.isInteger(tag) && tag != 0) tag = tag.toFixed(2);
-        if (colour === "white") {
-            styleString += `opacity: 75%;`;
-        }
-        if (player?.sources.runApi?.data.data.blacklist.tagged) {
-            styleString += `font-bold`;
-        }
-        if (tag !== undefined) {
-            if (typeof tag === "string") {
-                if (tag.length == 1) {
-                    // Makes tags have spaces
-                    classNameData += `pl-1 `;
-                }
-            } else if (typeof tag === "number") {
-                classNameData += `justify-center w-full`;
-                if (this.isOverlayStats) classNameData += ` flex`;
-            }
-            htmlResponse += `<span class="${classNameData}" style="${styleString}">${tag}</span>`;
-            return htmlResponse;
-        }
-        return htmlResponse;
-    };
 }
 
 export class PlayerHypixelUtils {
--- src/renderer/components/tooltips/StatisticsTooltip.tsx
+++ src/renderer/components/tooltips/StatisticsTooltip.tsx
@@ -5,8 +5,6 @@ import {OverlayTooltip} from "@components/tooltips/OverlayTooltip";
 import {Interweave} from "interweave";
 import RenderCoreStatsColour from "@common/utils/player/RenderCoreStatsColour";
 import RenderRatioColour from "@common/utils/player/RenderRatioColour";
-import useTagStore from "@renderer/store/zustand/TagStore";
-import useConfigStore from "@renderer/store/zustand/ConfigStore";
 
 export interface StatisticsTooltip {
     player: Player;
@@ -15,18 +13,17 @@ export interface StatisticsTooltip {
 
 export const StatsisticsTooltip: React.FC<PropsWithChildren<StatisticsTooltip>> = (props: StatisticsTooltip) => {
     const player = props.player;
-    const tagStore = useTagStore((state) => ({}));
-    const configStore = useConfigStore((state) => ({}))
-    const playerFormatter = new PlayerUtils({tags: tagStore, config: configStore});
+    const playerFormatter = new PlayerUtils();
 
-    let blacklistedReason = <span/>
+    let blacklistedReason = <span />;
     if (player.sources.runApi?.data.data.blacklist.tagged) {
         blacklistedReason = (
             <div>
-                <span>Reason:</span><br/>
-                <span className={'text-red-500'}>{player.sources.runApi.data.data.blacklist.reason}</span>
+                <span>Reason:</span>
+                <br />
+                <span className={"text-red-500"}>{player.sources.runApi.data.data.blacklist.reason}</span>
             </div>
-        )
+        );
     }
 
     return (
@@ -42,76 +39,81 @@ export const StatsisticsTooltip: React.FC<PropsWithChildren<StatisticsTooltip>> 
                                 </span>
                                 <div>
                                     <span className={"statistics-tooltip-inline"}>
-                                        Games Played: <RenderCoreStatsColour player={player} stats={'gamesPlayed'}/>{" "}
+                                        Games Played: <RenderCoreStatsColour player={player} stats={"gamesPlayed"} />{" "}
                                     </span>
                                 </div>
                                 <div>
                                     <span className={"statistics-tooltip-inline"}>
-                                        Wins: <RenderCoreStatsColour player={player} stat={'wins'}/>{" "}
+                                        Wins: <RenderCoreStatsColour player={player} stat={"wins"} />{" "}
                                     </span>
                                 </div>
                                 <div>
                                     <span className={"statistics-tooltip-inline"}>
-                                        Losses: <RenderCoreStatsColour player={player} stat={'losses'}/>{" "}{" "}
+                                        Losses: <RenderCoreStatsColour player={player} stat={"losses"} />{" "}
                                     </span>
                                 </div>
                                 <div>
                                     <span className={"statistics-tooltip-inline"}>
-                                        WLR: <RenderRatioColour player={player} ratio={'wlr'}/>{" "}
+                                        WLR: <RenderRatioColour player={player} ratio={"wlr"} />{" "}
                                     </span>
                                 </div>
-                                <br/>
+                                <br />
                                 <div>
                                     <span className={"statistics-tooltip-inline"}>
-                                        Final Kills: <RenderCoreStatsColour player={player} stat={'finalKills'}/>{" "}{" "}
-                                    </span>
-                                </div>
-                                <div>
-                                    <span className={"statistics-tooltip-inline"}>
-                                        Final Deaths: <RenderCoreStatsColour player={player} stat={'finalDeaths'}/>{" "}{" "}
+                                        Final Kills: <RenderCoreStatsColour player={player} stat={"finalKills"} />{" "}
                                     </span>
                                 </div>
                                 <div>
                                     <span className={"statistics-tooltip-inline"}>
-                                        FKDR: <RenderRatioColour player={player} ratio={'fkdr'}/>{" "}
-                                    </span>
-                                </div>
-                                <br/>
-                                <div>
-                                    <span className={"statistics-tooltip-inline"}>
-                                        Beds Broken: <RenderCoreStatsColour player={player} stat={'bedsBroken'}/>{" "}{" "}
+                                        Final Deaths: <RenderCoreStatsColour player={player} stat={"finalDeaths"} />{" "}
                                     </span>
                                 </div>
                                 <div>
                                     <span className={"statistics-tooltip-inline"}>
-                                        Beds Lost: <RenderCoreStatsColour player={player} stat={'bedsLost'}/>{" "}{" "}
+                                        FKDR: <RenderRatioColour player={player} ratio={"fkdr"} />{" "}
+                                    </span>
+                                </div>
+                                <br />
+                                <div>
+                                    <span className={"statistics-tooltip-inline"}>
+                                        Beds Broken: <RenderCoreStatsColour player={player} stat={"bedsBroken"} />{" "}
                                     </span>
                                 </div>
                                 <div>
                                     <span className={"statistics-tooltip-inline"}>
-                                        BBLR: <RenderRatioColour player={player} ratio={'bblr'}/>{" "}
-                                    </span>
-                                </div>
-                                <br/>
-                                <div>
-                                    <span className={"statistics-tooltip-inline"}>
-                                        Kills: <RenderCoreStatsColour player={player} stat={'kills'}/>{" "}
+                                        Beds Lost: <RenderCoreStatsColour player={player} stat={"bedsLost"} />{" "}
                                     </span>
                                 </div>
                                 <div>
                                     <span className={"statistics-tooltip-inline"}>
-                                        Deaths: <RenderCoreStatsColour player={player} stat={'deaths'}/>{" "}
+                                        BBLR: <RenderRatioColour player={player} ratio={"bblr"} />{" "}
+                                    </span>
+                                </div>
+                                <br />
+                                <div>
+                                    <span className={"statistics-tooltip-inline"}>
+                                        Kills: <RenderCoreStatsColour player={player} stat={"kills"} />{" "}
                                     </span>
                                 </div>
                                 <div>
                                     <span className={"statistics-tooltip-inline"}>
-                                        KDR: <RenderRatioColour player={player} ratio={'kdr'}/>{" "}
+                                        Deaths: <RenderCoreStatsColour player={player} stat={"deaths"} />{" "}
                                     </span>
                                 </div>
-                                <br/>
                                 <div>
                                     <span className={"statistics-tooltip-inline"}>
-                                        First Login: <Interweave content={playerFormatter.getPlayerHypixelUtils().getDateFormatted(player.hypixelPlayer.firstLogin)}/>{" "}
+                                        KDR: <RenderRatioColour player={player} ratio={"kdr"} />{" "}
+                                    </span>
+                                </div>
+                                <br />
+                                <div>
+                                    <span className={"statistics-tooltip-inline"}>
+                                        First Login: <Interweave content={playerFormatter.getPlayerHypixelUtils().getDateFormatted(player.hypixelPlayer.firstLogin)} />{" "}
+                                    </span>
+                                </div>
+                                <div>
+                                    <span className={"statistics-tooltip-inline"}>
+                                        Last Login: <Interweave content={playerFormatter.getPlayerHypixelUtils().getDateFormatted(player.hypixelPlayer.lastLogin)} />{" "}
                                     </span>
                                 </div>
                                 {blacklistedReason}
