ea0c61eff2c260e3c44c9aaa9fdf28aa1d7271a6
Arman <idocorrupt@gmail.com>
2022-09-26 23:36:04 -0400

    Fixed Astolfo logs not reading

--- src/main/appWindow.ts
+++ src/main/appWindow.ts
@@ -417,7 +417,7 @@ const registerLogCommunications = () => {
             });
 
             logFileReadline.on("line", async (line) => {
-                if (!line.includes("[Client thread/INFO]: [CHAT]") && !line.includes("[main/INFO]: [CHAT] ")) return;
+                if (!line.includes("[Client thread/INFO]: [CHAT]") && !line.includes("[main/INFO]: [CHAT] ") && !line.includes("[Astolfo HTTP Bridge]: [CHAT]")) return;
                 appWindow?.webContents.send("logFileLine", handleIPCSend<LogFileMessage>({ data: { message: line }, status: 200 }));
             });
         }
