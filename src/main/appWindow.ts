acb1c6ca1355ca94b371d6643a3d799ed0f11cba
Arman <idocorrupt@gmail.com>
2022-09-30 00:33:07 -0400

    Continued Astolfo

--- src/main/appWindow.ts
+++ src/main/appWindow.ts
@@ -421,7 +421,7 @@ const registerLogCommunications = () => {
                 if (line.includes("[Client thread/INFO]: [CHAT]") || line.includes("[main/INFO]: [CHAT] ")) {
                     appWindow?.webContents.send("logFileLine", handleIPCSend<LogFileMessage>({data: {message: line}, status: 200}));
                 } else if (line.includes("[Astolfo HTTP Bridge]: [CHAT]")) {
-                    const newLine = (line.replaceAll(/§[0-9A-FK-OR]/gi, "")).replaceAll(/�[0-9A-FK-OR]/gi, ""); // clean
+                    const newLine = (line.replaceAll(/\u00A7[0-9A-FK-OR]/gi, "")).replaceAll(/\uFFFD[0-9A-FK-OR]/gi, ""); // clean
                     appWindow?.webContents.send("logFileLine", handleIPCSend<LogFileMessage>({data: {message: newLine}, status: 200}));
                 } else {
                     return;
